32b2a260655d0996ae0f4a3e071147e190aaf110
Antonio Kim <antoniok@sfu.ca>
2024-03-22 05:14:13 -0700

    initial setup for Comment System

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GetAllAsync();
+            var commentDTO = comments.Select(s => s.ToCommentDTO());
+
+            return Ok(commentDTO);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IStockRepository, StockRepository>();
-
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
