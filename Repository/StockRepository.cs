42997674ded748e9afe392221710103ca8e6c649
Antonio Kim <antoniok@sfu.ca>
2024-03-22 11:34:32 -0700

    Adding Sorting and Pagination to GetAll()

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -50,8 +50,17 @@ namespace api.Repository
             {
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+            }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
