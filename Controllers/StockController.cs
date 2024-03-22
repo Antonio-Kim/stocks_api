7480f1a933debff26d5d3cc8dbd3bc71413444ac
Antonio Kim <antoniok@sfu.ca>
2024-03-22 04:30:57 -0700

    DELETE endpoint to api

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -67,5 +67,20 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDTO());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
